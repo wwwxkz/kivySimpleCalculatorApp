9600491d12ac12c82bcabba11c91d91de9699aeb
wwwxkz <wxkzwin@gmail.com>
2020-08-30 11:59:58 -0300

    App and Box classes name changed

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-#ARQUIVO PARA INTERFACE SEPARADO NOMEDACLASSPRINCIPAL.KV ########################################
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
-class Janela(BoxLayout):
+class Window(BoxLayout):
 	pass 
 
-class Djaime(App):
+class Calculator(App):
 	def build(self):
-		return Janela()
+		return Window()
 
-Djaime().run()
+Calculator().run()
